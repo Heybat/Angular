48a59131a7f0fa429a492a135e88d8a47bc3509f
Heybat Guliyev <heybat@mail.ru>
2018-04-26 09:48:38 +0400

    first commit

---
+++ shop/shop/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDY9s416u_KCJigCJUXTMo4cU22SF54GVE",
+    authDomain: "shop-8339f.firebaseapp.com",
+    databaseURL: "https://shop-8339f.firebaseio.com",
+    projectId: "shop-8339f",
+    storageBucket: "shop-8339f.appspot.com",
+    messagingSenderId: "908759685793"
+  }
+};
